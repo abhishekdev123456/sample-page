43a281f26ca5881e24abfa41ed7297eda1379239
Abhishek Sharma <abhishekvatts9@gmail.com>
2018-12-13 12:46:33 -0800

    'latest'

--- index.php
+++ index.php
@@ -1,107 +1,207 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Demo</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
-  </head>
-
-  <body id="page-top">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark  fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Test Weblify</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto mr-70">
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#services">SERVICES</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#chemistry">CHEMISTRY</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#projects">PROJECTS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">ABOUT US</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">CONTACT</a>
-            </li>
-          </ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Weblify</title>
+    <link rel='stylesheet' href='./css/bootstrap/css/bootstrap.min.css'>
+    <link rel='stylesheet'  href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'>
+    <link rel="stylesheet" href='./css/styles.css'>
+</head>
+<body>
+    <!-- navbar -->
+    <header class="m-bg">
+        <div class="m-bg-dark">
+                <nav class="navbar navbar-expand-lg navbar-dark  py-lg-4">
+                        <a class="navbar-brand text-lg-center flex-grow-1 js-scroll-trigger" href="#">Test Weblify</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item ">
+                            <a class="nav-link text-white js-scroll-trigger" href="#services">Services</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link text-white js-scroll-trigger" href="#chemistry">Chemistry</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link  text-white js-scroll-trigger" href="#">Projects</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link  text-white js-scroll-trigger" href="#">About Us</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link  text-white js-scroll-trigger" href="#">Contact</a>
+                            </li>
+                        </ul>
+                        </div>
+                    </nav>
+            
+            <div class="col-12">
+                <h6 class="text-center m-padd-top text-white">ANOTHER LINE OF INFORMATION</h6>
+                <p class="m-p-head offset-1 col-10 text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div class="col-12 text-center m-margin-btns">
+                <button class="btn m-btn-colored text-white m-btn mx-3">LOOK AT IT</button>
+                <button class="btn btn-outline-light m-btn">CONTACT US</button>
+            </div>
         </div>
-      </div>
-    </nav>
-
-    <header class="text-white" style="background-color: black;">
-      <div class="container-fluid text-center"  id="header" >
-          <div class="container header-content">
-              <h1>Welcome</h1>
-              <p class="lead">A landing page template.</p>
-          </div>
-      </div>   
     </header>
 
-    <section id="about">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h2>About this page</h2>
-            <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
-            <ul>
-              <li>Clickable nav links that smooth scroll to page sections</li>
-              <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-              <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-              <li>Minimal custom CSS so you are free to explore your own unique design options</li>
-            </ul>
-          </div>
+    <!-- services -->
+    <section id='services'>
+        <h2 class="text-center m-margin-service-top">SERVICES</h2>
+        <p class="text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
+        <div class="m-title-boxes">
+            <div class=" col-md-12 col-lg-8 offset-lg-2"> 
+                <div class="row">   
+                    <div class="col-8 offset-2 col-sm-4 offset-sm-0 my-2 my-sm-0">
+                        <div class="card" style="width: inherit;">
+                                    <img class="card-img-top" src="./images/Luxury Villa.webp" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Title 1</h5>
+                                    <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                                    <a href="#" class='m-read-more-color'>Read More</a>
+                                    </div>
+                        </div>
+                    </div>
+                    <div class="col-8 offset-2 col-sm-4 offset-sm-0 my-2 my-sm-0">
+                        <div class="card" style="width: inherit">
+                                    <img class="card-img-top" src="./images//VR Headset.webp" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Title 2</h5>
+                                    <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                                    <a href="#" class="m-read-more-color">Read More</a>
+                                    </div>
+                        </div>
+                    </div>
+                    <div class="col-8 offset-2 col-sm-4 offset-sm-0 my-2 my-sm-0">
+                        <div class="card" style="width: inherit">
+                                    <img class="card-img-top" src="./images/Audience.webp" alt="Card image cap">
+                                    <div class="card-body">
+                                    <h5 class="card-title">Title 3</h5>
+                                    <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                                    <a href="#" class="m-read-more-color">Read More</a>
+                                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="text-center">
+        <button class="btn m-btn-contact-us m-btn text-white m-margin-btns">CONTACT US</button>
+        </div>
+    </section>
+    <!-- testimonials -->
+    <section class="m-testimonial-margin">
+                <div class="col-md-12 col-lg-8 offset-lg-2"> 
+                    <div class="row">   
+                        <div class="col-8 offset-2 col-sm-4 offset-sm-0 my-2 my-sm-0">
+                                <div class="m-testimonials" style="width: inherit; margin-right:25px;">
+                                        <div class="text-center">    
+                                            <img class="m-testimonial-card-img-top text-center" src="./images/Woman Working in Modern Office.webp" alt="Card image cap" style="width: 35%;height: 90px;">
+                                        </div>
+                                        <div class="card-body">
+                                        <h5 class="card-title text-center">Ana Storm</h5>
+                                        <p class="card-text text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                                        <p class="m-read-more-color text-center" style="font-size: 30px;">
+                                                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                        </p>
+                                        </div>
+                            </div>
+                        </div>
+                        <div class="col-8 offset-2 col-sm-4 offset-sm-0 my-2 my-sm-0">
+                                <div class="m-testimonials" style="width: inherit; margin-right:25px;">
+                                        <div class="text-center">
+                                            <img class="m-testimonial-card-img-top" src="./images/leende flicka.webp" alt="Card image cap" style="width: 35%;height: 90px;">
+                                        </div>
+                                        <div class="card-body">
+                                        <h5 class="card-title text-center">Jane Faber</h5>
+                                        <p class="card-text text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                                        <p class="m-read-more-color text-center" style="font-size: 30px;">
+                                                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                        </p>
+                                        </div>
+                            </div>
+                        </div>
+                        <div class="col-8 offset-2 col-sm-4 offset-sm-0 my-2 my-sm-0">
+                                <div class="m-testimonials" style="width: inherit; margin-right:25px;">
+                                        <div class="text-center">
+                                            <img class="m-testimonial-card-img-top" src="./images/Leende tonårspojke.webp" alt="Card image cap" style="width: 35%;height: 90px;">
+                                        </div>
+                                        <div class="card-body">
+                                        <h5 class="card-title text-center">Johan Skog</h5>
+                                        <p class="card-text text-center">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+                                        <p class="m-read-more-color text-center" style="font-size: 30px;">
+                                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                        </p>
+                                        </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </section>
+    <!-- advertisement -->
+    <section class="m-advertisement-margin-top">
+        <div class="col-12 col-md-8 offset-md-2">
+            <div class="row">
+                
+                    <div class="col-sm-3 col-lg-2 text-center" style="height: 70px; line-height: 70px;">
+                        <img src="./images/samsung.webp">
+                    </div>
+                    <div class="col-sm-3 col-lg-2 text-center" style="height: 70px; line-height: 70px;">
+                        <img src="./images/toshiba.webp">
+                    </div>
+                    <div class="col-sm-3 col-lg-2 text-center" style="height: 70px; line-height: 70px;">
+                        <img src="./images/nibe-logo.webp">
+                    </div>
+                    <div class="col-sm-3 col-lg-2 text-center" style="height: 70px; line-height: 70px;">
+                        <img src="./images/lg.webp">
+                    </div>
+                    <div class="col-sm-3 col-lg-2 text-center" style="height: 70px; line-height: 70px;">
+                        <img src="./images/ivt.webp">
+                    </div>
+                    <div class="col-sm-3 col-lg-2 text-center" style="height: 70px; line-height: 70px;">
+                        <img src="./images/mistubushi.webp">
+                    </div>
+                
+            </div>
+        </div>
     </section>
 
-    <section id="services" class="bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h2>Services we offer</h2>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
-          </div>
+    <!-- last section -->
+    <section class="m-last-section-top" id="chemistry">
+        <div class="col-lg-10 offset-lg-1">
+            <div class="row">
+                <div class="col-12 col-sm-4 col-lg-6">
+                    <img src="./images/last-one.webp" style="object-fit: cover;">
+                </div>
+                <div class="col-12 col-sm-8 col-lg-5 offset-lg-1">
+                    <h6  style=" color:#938ABB;">UPPERCASE TEXT</h6>
+                    <h1>
+                            Sed ut perspiciatis unde omnis iste natus error 
+                    </h1>
+                    <p>
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo
+                    </p>
+                    <button class="btn m-btn m-btn-colored text-white">Read More</button>
+                    <a href='#' class="m-read-more-color" style="margin-left: 30px;">Take a Survey</a>
+                </div>
+            </div>
         </div>
-      </div>
+    </section>
+    <!-- footer -->
+    <section style="margin-top:40px;">
+    <div style="background-color:#000; padding: 10px;" >
+        <p class="text-center text-white">A test of weblift</p>
+    </div>
     </section>
 
-    <section id="contact">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h2>Contact us</h2>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/custom.js"></script>
-
-  </body>
-
+    <script src="./css/bootstrap/js/jquery.min.js"></script>
+    <script src="./css/bootstrap/js/bootstrap.min.js"></script>
+    <script src="./css/bootstrap/js/jquery.easing.min.js"></script>
+    <script src='./css/bootstrap/js/custom.js'></script>
+</body>
 </html>
